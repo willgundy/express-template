807a45fa17fe975d9aceeb9490b4248c721ec430
Will Gundy <willgunderson503@gmail.com>
2022-08-09 11:03:09 -0700

    added try catch to setupDb file

--- setup-heroku.js
+++ setup-heroku.js
@@ -1,9 +1,15 @@
-const pool = require('../lib/utils/pool');
 const { readFileSync } = require('node:fs');
+const pool = require('./lib/utils/pool');
 const sql = readFileSync('./sql/setup.sql', 'utf-8');
 
 async function setupDb() {
-  return await pool.query(sql);
+  try {
+    const response = await pool.query(sql);
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
 }
+
 
 setupDb();

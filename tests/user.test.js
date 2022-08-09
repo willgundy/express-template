183ff365a66cd220ac73877dfbe9d284228251ae
Will Gundy <willgunderson503@gmail.com>
2022-08-08 17:59:48 -0700

    got sign in test working

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,6 @@
 const { setupDb, signUpUser } = require('./utils.js');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('/api/v1/user', () => {
   beforeEach(setupDb);
@@ -18,6 +18,7 @@ describe('/api/v1/user', () => {
   });
 
   it('/signup with duplicate email', async () => {
+    //calling it twice so that there are two users with same email
     await signUpUser();
     const { res } = await signUpUser();
     expect(res.statusCode).toBe(400);
@@ -25,5 +26,22 @@ describe('/api/v1/user', () => {
       status: 400,
       message: 'Email already exists',
     });
+  });
+
+  it('/signin', async () => {
+    const { credentials } = await signUpUser();
+
+    console.log(credentials);
+
+    const agent = request.agent(app);
+    const res = await agent.post('/api/v1/user/signin').send(credentials);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: credentials.email,
+    });
+
+    const { statusCode } = await agent.get('/api/v1/user/verify');
+    expect(statusCode).toBe(200);
   });
 });

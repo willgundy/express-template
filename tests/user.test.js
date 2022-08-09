9ed1ea81b0ce77e3cc7a24bb4080ae98914495d3
Will Gundy <willgunderson503@gmail.com>
2022-08-08 18:04:36 -0700

    got password and email check test working

--- tests/user.test.js
+++ tests/user.test.js
@@ -31,8 +31,6 @@ describe('/api/v1/user', () => {
   it('/signin', async () => {
     const { credentials } = await signUpUser();
 
-    console.log(credentials);
-
     const agent = request.agent(app);
     const res = await agent.post('/api/v1/user/signin').send(credentials);
 
@@ -43,5 +41,31 @@ describe('/api/v1/user', () => {
 
     const { statusCode } = await agent.get('/api/v1/user/verify');
     expect(statusCode).toBe(200);
+  });
+
+  it('/signin bad email, bad password', async () => {
+    const { credentials } = await signUpUser();
+
+    const res = await request(app)
+      .post('/api/v1/user/signin')
+      .send({ ...credentials, email: 'bad@email.com' });
+
+    console.log(res.body);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual({
+      status: 400,
+      message: 'Invalid credentials',
+    });
+
+    const res2 = await request(app)
+      .post('/api/v1/user/signin')
+      .send({ ...credentials, password: 'badpassword' });
+
+    expect(res2.statusCode).toBe(400);
+    expect(res2.body).toEqual({
+      status: 400,
+      message: 'Invalid credentials',
+    });
   });
 });

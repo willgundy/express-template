971ebba52a4626110f4461aea798fcf527286b58
Will Gundy <willgunderson503@gmail.com>
2022-08-08 17:55:11 -0700

    duplicate email test working

--- tests/user.test.js
+++ tests/user.test.js
@@ -16,4 +16,14 @@ describe('/api/v1/user', () => {
     const { statusCode } = await res.agent.get('/api/v1/user/verify');
     expect(statusCode).toBe(200);
   });
+
+  it('/signup with duplicate email', async () => {
+    await signUpUser();
+    const { res } = await signUpUser();
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual({
+      status: 400,
+      message: 'Email already exists',
+    });
+  });
 });

23cf4431e866aef3ca27949bd2496db648616093
Will Gundy <willgunderson503@gmail.com>
2022-08-08 19:09:31 -0700

    got the get all and get id working

--- tests/item.test.js
+++ tests/item.test.js
@@ -20,4 +20,28 @@ describe('/api/v1/items', () => {
       created_at: expect.any(String)
     });
   });
+  
+  it('GET / returns all items associated with the authenticated User', async () => {
+    const { agent } = await signUpUser();
+    const { body: user1Item } = await agent.post('/api/v1/items').send({
+      description: 'apples'
+    });
+
+    const { agent: agent2 } = await signUpUser({
+      email: 'user2@email.com',
+      password: 'password',
+    });
+
+    const { body: user2Item } = await agent2.post('/api/v1/items').send({
+      description: 'eggs'
+    });
+
+    const resp1 = await agent.get('/api/v1/items');
+    expect(resp1.status).toEqual(200);
+    expect(resp1.body).toEqual([user1Item]);
+
+    const resp2 = await agent2.get('/api/v1/items');
+    expect(resp2.status).toEqual(200);
+    expect(resp2.body).toEqual([user2Item]);
+  });
 });
